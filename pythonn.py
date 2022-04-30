0659ff3a04b135b72cb88fb574347ef0e22ab446
Luis Eduardo <luiswag.20001@gmail.com>
2022-04-30 18:38:44 -0500

    notas de python

---
+++ pythonn.py
@@ -0,0 +1,22 @@
+# input and print
+
+name = input("Insert tu nombre : ")
+print("hello, "+name)
+print(f"hello, {name} ")
+
+
+
+# Conditions
+
+num = input("Numero: ")
+n=int(num)
+if n > 0:
+    print("num es positivo")
+elif n < 0:
+    print("num is negative")
+else:
+    print("num is zero")
+    
+    
+    
+# otra
