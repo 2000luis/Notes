c5bb8b8fda23336fb27cb2327df4610b6cc462e6
Luis Eduardo <luiswag.20001@gmail.com>
2022-04-30 19:18:33 -0500

    add secuencias

---
+++ python.py
@@ -0,0 +1,29 @@
+# input and print
+
+name = input("Insert tu nombre : ")
+print("hello, "+name)
+print(f"hello, {name} ")
+
+
+
+# Conditions
+
+num = input("Numero: ")
+n=int(num)
+if n > 0:
+    print("num es positivo")
+elif n < 0:
+    print("num is negative")
+else:
+    print("num is zero")
+    
+    
+    
+# secuencias
+
+nombre = "harry"
+print(nombre[0])
+
+names= ["harry", "ron"]
+print(names)
+print(names[0])
