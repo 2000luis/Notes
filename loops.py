c912f0a27752b0b5c3c6b73a625ae3b429b68d36
Luis Eduardo <luiswag.20001@gmail.com>
2022-04-30 23:07:44 -0500

    notas de loops

---
+++ loops.py
@@ -0,0 +1,17 @@
+
+#Que la variable i pase por toda la list y que imprima hola
+# for i in [0,2,3]:
+#     print("hola")
+    
+# for j in range(6):
+#     print(j)
+    
+    
+names = ["harry","Ron", "luis"]
+
+for name in names:
+    print(name)
+    
+name= "luis"
+for j in name:  
+    print(j)
