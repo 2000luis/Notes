b66d86359eb27801c3e9755389c1c3b205e509a4
Luis Eduardo <luiswag.20001@gmail.com>
2022-05-02 13:12:00 -0500

    notas de dictionaries

---
+++ dictionarys.py
@@ -0,0 +1,8 @@
+#dictionaries
+houses = {"Luis":"la verga","Anthony":"el wey"}
+
+#Asi es como agregas otro elemento un dictionarie
+houses["jaque"]="her"
+
+#asi es como llamas un elemento de un dictionarie
+print(houses["jaque"])
