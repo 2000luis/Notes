27c36569b675766c1740d2afe497de246d8123e3
Luis Eduardo <luiswag.20001@gmail.com>
2022-05-02 14:23:19 -0500

    Otra forma de importar

--- funtions.py
+++ funtions.py
@@ -1,6 +1,11 @@
-# asi se importa una funcion de otro file 
-from square import square
+#asi se importa una funcion de otro file 
+#from square import square
+
+#Otra forma de importar las funciones es de 
+
+import square
+
 
 #la funcion esta en el file square.py
 for i in range(11):
-    print(f"el cuadrado de {i} es {square(i)} ")
+    print(f"el cuadrado de {i} es {square.square(i)} ")
