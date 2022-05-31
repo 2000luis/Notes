814ba81db9997f809e92bafcf949a7531f0d8ab0
Luis Eduardo <luiswag.20001@gmail.com>
2022-05-31 13:03:29 -0500

    add counter with alert

--- dictionarys.py
+++ dictionarys.py
@@ -6,3 +6,35 @@ houses["jaque"]="her"
 
 #asi es como llamas un elemento de un dictionarie
 print(houses["jaque"])
+
+j={'luis':1,'juan':4,'sara':3,}
+print(list(j))
+#Te sale los keys en una lista
+#['luis', 'juan', 'sara']
+
+print(j.keys())
+#Te regresa lo mismo que list los nombre de los keys
+#['luis', 'juan', 'sara']
+
+print(j.values())
+#Te regresa los valores de los keys
+#[1,4,3]
+
+
+print(j.items())
+#Esta forma te regresa un tipo de tuple como una lista pero con tuples a dentro
+#[('luis',1) ('juan',4) ('sara',3)]
+
+
+for keyllave,valuevalor in j.items():
+    print(keyllave,valuevalor)
+    
+#['luis', 'juan', 'sara']
+#dict_values([1, 4, 3])
+#dict_keys(['luis', 'juan', 'sara'])
+#dict_items([('luis', 1), ('juan', 4), ('sara', #3)])
+#luis 1
+#juan 4
+#sara 3
+
+#dict() dicionario vacio
