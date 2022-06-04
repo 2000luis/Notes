6e227205342b4eff05edfe876ad1fb6e809cf88a
Luis Eduardo <luiswag.20001@gmail.com>
2022-06-04 09:59:49 -0500

    api's currency

--- javascript_html/counter.js
+++ javascript_html/counter.js
@@ -1,16 +1,23 @@
-let counter = 0;
+
+if (!localStorage.getItem('counter')) {
+    localStorage.setItem('counter', 0);
+}
 function count() {
-    counter += 1;
+
+    let counter = localStorage.getItem('counter');
+    counter++;
     const h1 = document.querySelector('h1');
     h1.innerHTML = counter;
+    localStorage.setItem('counter', counter);
     //if (counter % 10 === 0) {
     //  alert(`The counter is now: ${counter}`);
     //}
 }
 document.addEventListener('DOMContentLoaded', function () {
+    document.querySelector('h1').innerHTML = localStorage.getItem('counter');
     document.querySelector('button').onclick = count;
 
-    setInterval(count, 1000);
+    //setInterval(count, 1000);
     //Esto hace intervalos de un segundo el contador estara contando sin necesidad de presionar el boton 
 });
 
